8f8a580d1f7e9522283da568f09f1f9ed0070455
acharayaP03 <acharyaP03@gmail.com>
2023-03-02 17:32:40 +1100

    Feedback section

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -4,7 +4,7 @@ import { fadeIn } from "../utils/motion";
 
 const InsightCard = ({imgUrl, title, subtitle, index }) => (
   <motion.div
-    variant={fadeIn('up', 'spring', index * 0.5, 1)}
+    variants={fadeIn('up', 'spring', index * 0.5, 1)}
     className="flex md:flex-row flex-col gap-4"
   >
     <img src={imgUrl} alt="planet-01" className="md:w-[270px]  w-full h-[250px] rounded-[32px] object-cover"/>
