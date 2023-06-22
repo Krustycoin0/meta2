5665dd9988ecab8e0d6db89f6521b52a441204fe
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2023-06-23 01:10:47 +0200

    Update Hero.jsx

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -26,8 +26,9 @@ const Hero = () => (
         >
             <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]"/>
           <div>
-                <a href="https://www.pinksale.finance/launchpad/0x6b29A570A03a79237Df01649726378116367c9c2?chain=Matic&refId=0x9B89c6f9e6C5DfcBDa3E5b85563Dd2808dE62207">
-            <img src="/cover.png" alt="cover" className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"/>
+             
+		  <h2><li><a title="Win 50000 matic" href="https://www.pinksale.finance/launchpad/0x6b29A570A03a79237Df01649726378116367c9c2?chain=Matic&refId=0x9B89c6f9e6C5DfcBDa3E5b85563Dd2808dE62207" target="_blank">
+	      <img src="/cover.png" alt="cover" className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"/>
             <a href="#explore">
                 <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
                     <img src="/stamp.png" alt="stamp" className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"/>
